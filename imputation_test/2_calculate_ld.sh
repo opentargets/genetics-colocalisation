1049f560ec3a687eba2a14de736eec1ccab12a29
edm1 <emountjoy@gmail.com>
2019-03-20 16:10:07 +0000

    calc ld

--- imputation_test/2_calculate_ld.sh
+++ imputation_test/2_calculate_ld.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 # Args
 script='scripts/calc_ld_1000G.py'
-in_ld='/home/em21/genetics-v2d-data/tmp/190315/ld/1000Genomep3/EUR/EUR.CHROM.1000Gp3.20130502'
+in_ld='/home/em21/genetics-colocalisation/imputation_test/input_data/uk10k/CHROM.ALSPAC_TWINSUK.maf01.beagle.csq.shapeit.20131101'
 in_varlist='input_data/var_list.tsv'
 out_dir='ld_output'
 
@@ -19,11 +19,10 @@ cat $in_varlist | while read line; do
     var_id=${arr[0]}_${arr[1]}_${arr[2]}_${arr[3]}
 
     # Make command
-    bfile=${in_ld/CHROM/${arr[0]}}
     outf="$out_dir/$var_id.ld.tsv.gz"
     echo python $script \
       --varid $var_id \
-      --bfile $bfile \
+      --bfile $in_ld \
       --ld_window 1000000 \
       --min_r2 0.7 \
       --outf $outf
