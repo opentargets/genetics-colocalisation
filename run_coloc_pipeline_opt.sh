5005d0e993cacec9814ec9878293a03dbc896a0e
Ruslan Forostianov <ruslan@thehyve.nl>
2020-02-27 16:16:38 +0100

    Shuffle commands in random order
    for mixing processes that require much RAM and those that do not.
    We can run out of memory when there are several processes that take
    much RAM running at the same time.

--- run_coloc_pipeline_opt.sh
+++ run_coloc_pipeline_opt.sh
@@ -2,7 +2,7 @@
 
 bash 1_find_overlaps.sh
 python 2_generate_manifest.py
-python make_prepare_commands.py | parallel -j 40
-python 3_make_commands_opt.py | parallel -j 40
+python make_prepare_commands.py | shuf | parallel -j 40
+python 3_make_commands_opt.py | shuf | parallel -j 40
 python 5_combine_results.py
 python 6_process_results.py
