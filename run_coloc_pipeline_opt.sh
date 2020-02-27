8903a272f12ad2277d3bc8b01cb743db0333bb30
Ruslan Forostianov <ruslan@thehyve.nl>
2020-02-27 16:13:41 +0100

    Use more corese for eqtl subsetting
    eqtl parquet has to be partitioned by chrom at least

--- run_coloc_pipeline_opt.sh
+++ run_coloc_pipeline_opt.sh
@@ -2,8 +2,7 @@
 
 bash 1_find_overlaps.sh
 python 2_generate_manifest.py
-python make_prepare_commands.py --type eqtl | parallel -j 2
-python make_prepare_commands.py --type gwas | parallel -j 40
+python make_prepare_commands.py | parallel -j 40
 python 3_make_commands_opt.py | parallel -j 40
 python 5_combine_results.py
 python 6_process_results.py
