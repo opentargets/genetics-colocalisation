0433efc23a24fad6e52854e83cc729f3424d7f88
Jeremy Schwartzentruber <jeremy37@gmail.com>
2021-03-25 23:09:50 +0000

    Added options to run steps 3a and 3b with --quiet, to see which commands are done / todo.

--- run_coloc_pipeline_opt.sh
+++ run_coloc_pipeline_opt.sh
@@ -2,7 +2,12 @@
 
 bash 1_find_overlaps.sh
 python 2_generate_manifest.py
-python make_prepare_commands.py | shuf | parallel -j 40
-python 3_make_commands_opt.py | shuf | parallel -j 40
+
+python 3a_make_conditioning_commands.py --quiet
+python 3a_make_conditioning_commands.py | shuf | parallel -j 40
+
+python 3b_make_coloc_commands.py --quiet
+python 3b_make_coloc_commands.py | shuf | parallel -j 40
+
 python 5_combine_results.py
 python 6_process_results.py
